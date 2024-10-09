068f20b69c8709682583cadc6bb4bd9c4001d82a
Borregacion <mario.borrego100@gmail.com>
2024-10-09 09:24:12 +0200

    Optimización del codigo y extración de urls a application.yml

--- src/main/java/com/marioborrego/acortadorurlbackend/AcortadorUrlBackEndApplication.java
+++ src/main/java/com/marioborrego/acortadorurlbackend/AcortadorUrlBackEndApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcortadorUrlBackEndApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AcortadorUrlBackEndApplication.class, args);
     }
-
 }
--- src/main/java/com/marioborrego/acortadorurlbackend/controllers/UrlController.java
+++ src/main/java/com/marioborrego/acortadorurlbackend/controllers/UrlController.java
@@ -3,10 +3,10 @@ package com.marioborrego.acortadorurlbackend.controllers;
 import com.marioborrego.acortadorurlbackend.service.UrlService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -15,6 +15,15 @@ import java.util.Optional;
 public class UrlController {
     private final UrlService urlService;
     private final Logger logger = LoggerFactory.getLogger(UrlController.class);
+
+    @Value("${app.url.shortener.domain}")
+    private String dominio;
+
+    @Value("${app.url.shortener.URL_PATH}")
+    private String URL_PATH;
+
+    @Value("${app.url.shortener.REDIRECT_URL_PATH}")
+    private String REDIRECT_URL_PATH;
 
     public UrlController(UrlService urlService) {
         this.urlService = urlService;
@@ -26,29 +35,24 @@ public class UrlController {
     }
 
     @GetMapping("obtenerUrlOriginal/{uuid}")
-    public ResponseEntity<Map<String,String>> obtenerUrlOriginal(@PathVariable String uuid) {
+    public ResponseEntity<Map<String, String>> obtenerUrlOriginal(@PathVariable String uuid) {
         Optional<String> originalUrl = urlService.obtenerUrlOriginal(uuid);
-        Map<String,String> url = new HashMap<>();
-        if (originalUrl.isPresent()) {
-            url.put("url", originalUrl.get());
+
+        return originalUrl.map(url -> {
             logger.info("URL encontrada para {}", uuid);
-            logger.info("Redirigiendo a {}", url);
-            return ResponseEntity.status(200).body(url);
-        } else {
+            return ResponseEntity.ok(Map.of("url", url));
+        }).orElseGet(() -> {
             logger.warn("URL no encontrada para {}", uuid);
-            url.put("url", "http://localhost:4200/acortar");
-            return ResponseEntity.status(404).body(url);
-        }
+            return ResponseEntity.status(404).body(Map.of("url", dominio + REDIRECT_URL_PATH));
+        });
     }
 
     @PostMapping("obtenerUrlCorta")
     public ResponseEntity<Map<String, String>> obtenerUrlCorta(@RequestBody Map<String, String> request) {
-        String url = request.get("url"); // La URL se pasa en el cuerpo de la petición
+        String url = request.get("url");
         logger.info("Generando URL corta para {}", url);
         String shortUrl = urlService.generarUrlCorta(url);
-        Map<String, String> response = new HashMap<>();
-        String dominio = "http://localhost:4200/obtenerUrlOriginal/";
-        response.put("urlCorta", dominio + shortUrl);
-        return ResponseEntity.status(200).body(response);
+
+        return ResponseEntity.ok(Map.of("urlCorta", dominio + URL_PATH + shortUrl));
     }
 }
